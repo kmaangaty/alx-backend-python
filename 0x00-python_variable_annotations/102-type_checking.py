20352c4199cafe292cc576865760d75bebd7c747
Kmangaty <kmaangaty@gmail.com>
2024-05-03 03:04:50 +0300

    commit for setup

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
-"""Task 12: Type Checking
 """
-from typing import List, Tuple, Any
+Module for zooming in on a tuple.
+"""
+from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
-    """Creates multiple copies of items in a tuple and returns them as a list.
-
-    Args:
-        lst (Tuple): The tuple containing items to be copied.
-        factor (int, optional): The number of copies to create for each item. Defaults to 2.
-
-    Returns:
-        List: A list containing multiple copies of items from the input tuple.
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    zoomed_in: List[Any] = [
+       Zooms in on the elements of a tuple by repeating each element a certain number of times.
+
+       Args:
+           lst (Tuple): The tuple containing elements to be zoomed in on.
+           factor (int, optional): The factor by which to zoom in. Defaults to 2.
+
+       Returns:
+           List: A list containing the zoomed-in elements.
+    """
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
 
 
-array: Tuple[int, int, int] = (12, 72, 91)
+array = (12, 72, 91)
 
-zoom_2x: List[int] = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-zoom_3x: List[int] = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3)
