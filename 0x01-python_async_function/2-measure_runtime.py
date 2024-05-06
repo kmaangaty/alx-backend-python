37b2bdce691f3e1b4e20f6b0be9bb227b35a5ca2
Kmangaty <kmaangaty@gmail.com>
2024-05-06 19:34:54 +0300

    commit for setup

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""Contains a function that measures time to run wait_n"""
-import time
+"""2nd Task"""
+
 import asyncio
+import time
+from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
-# If time.perf_counter() doesn't work change it to time.time()
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-    """Measures time to run wait_n"""
-    s = time.perf_counter()
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """ You will spawn wait_random n times with the specified max_delay """
+    time_start = time.time()
     asyncio.run(wait_n(n, max_delay))
-    elapsed = time.perf_counter() - s
-
-    return elapsed/n
+    time_end = time.time()
+    return (time_end - time_start)/n
